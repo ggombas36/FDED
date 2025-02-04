296b5d1f09e36feb5d4681908d12243d7117019b
ggombas36 <113374490+ggombas36@users.noreply.github.com>
2025-02-04 21:48:50 +0100

    FEATURE - create footer

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="footer-container">
+        <p>Copyright © 2025 FDed | Powered by FDed</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+// No specific script needed
+</script>
+
+<style scoped>
+.footer-container {
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #ccc;
+}
+</style>
